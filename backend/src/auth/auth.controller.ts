3b093722d39d732b285a53a4ccaa39530f46434b
nishioka-shinji <s61nov11.shinji.nishioka@gmail.com>
2023-05-15 06:31:20 +0900

    csrf-tokenの実装

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -15,7 +15,12 @@ import { Csrf, Msg } from './interfaces/auth.interface';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(private readonly authService: AuthService) { }
+
+  @Get('csrf')
+  getCsrfToken(@Req() req: Request): Csrf {
+    return { csrfToken: req.csrfToken() };
+  }
 
   @Post('signup')
   signup(@Body() dto: AuthDto): Promise<Msg> {
